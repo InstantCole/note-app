1ff7dcdbad00bd2e715995dacca3743794895c31
Cole <cole.reichenberg@gmail.com>
2021-06-18 21:50:28 -0600

    We have liftoff

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+const firstElement = <p>This is a test</p>
+
+ReactDom.render(firstElement, document.getElementById("app"))
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+        }]
+    }
+}
