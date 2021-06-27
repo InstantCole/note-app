a26ceff3c37231a6aadcd7e39d9ae6a911fc4d53
Cole <cole.reichenberg@gmail.com>
2021-06-27 15:13:26 -0600

    added webpack css plugins

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import App from './app.js'
-
+import './styles/style.scss'
 
 ReactDom.render(<App/>, document.getElementById("app"))
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
+    mode: 'development',
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.join(__dirname, 'dist'),
         filename: 'main.js'
     },
     module: {
@@ -11,6 +13,20 @@ module.exports = {
             loader: 'babel-loader',
             test: /\.js$/,
             exclude: /node_modules/
+        },{
+            test: /\.s?css$/i,
+            use: [
+                MiniCssExtractPlugin.loader,
+                'css-loader',
+                'sass-loader'
+            ]
         }]
-    }
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist')
+    },
+    plugins: [new MiniCssExtractPlugin({
+        insert: '#bunldedstyles',
+        filename: 'bundledstyles.css'
+    })]
 }
